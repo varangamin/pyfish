241e4785ba2a0dc9919ac14d8e5f6eef89e8fb68
meeker.brian <meeker.brian@12549c7e-fc75-11dd-ada4-5153b8187bf2>
2009-03-18 02:28:21 +0000

    refs #1  * Added the GPL text as COPYING  * Added a copyright and GPL notice in each code file.

--- Map.py
+++ Map.py
@@ -1,3 +1,20 @@
+#Copyright 2009 Brian Meeker
+#
+#This file is part of pyFish.
+#
+#pyFish is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#pyFish is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with pyFish.  If not, see <http://www.gnu.org/licenses/>.
+
 class Map:
     
     def __init__(self, continents):
