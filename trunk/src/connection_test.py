c59e367188423c51a35eec71929f95173ff815d9
meeker.brian <meeker.brian@12549c7e-fc75-11dd-ada4-5153b8187bf2>
2009-06-03 01:34:53 +0000

    Figured out how to properly import and create Rules objects.

--- trunk/src/connection_test.py
+++ trunk/src/connection_test.py
@@ -1,27 +1,29 @@
+#Copyright 2009 Brian Meeker
+#
+#This file is part of pyFish.
+#
+#pyFish is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#pyFish is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with pyFish.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This file is for me to experiment with different json processing methods in Python."""
+
 import urllib.request
 import json
-
-class Rules:
-
-    def __init__(self, numAttacks, numTransfers):
-        self._numAttacks = numAttacks
-        self._numTransfers = numTransfers
-        
-    @property
-    def numAttacks(self):
-        """The number of attacks that are allowed per turn.
-        A value of -1 means an infinite number are allowed."""
-        return self._numAttacks
+from pyFish import Rules
     
-    @property
-    def numTransfers(self):
-        """The number of transfers that are allowed per turn."""
-        return self._numTransfers
-    
-
 def as_rules(dct):
     if 'numattacks' in dct:
-        return Rules(dct['numattacks'], dct['numtransfers'])
+        return Rules.Rules(dct['numattacks'], dct['numtransfers'])
     return dct
 
 response = urllib.request.urlopen('http://warfish.net/war/services/rest?_method=warfish.tables.getDetails&gid=55808245&_format=json')
@@ -30,3 +32,4 @@ print(json.dumps(bytes.decode(html), sort_keys=True, indent=4))
 rules = json.loads(bytes.decode(html), object_hook=as_rules)
 
 print(rules['_content']['rules'].numAttacks)
+print(rules)
