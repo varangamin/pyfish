1b3e6fc61a0111482102c785aeeacf25d3b1a847
meeker.brian <meeker.brian@12549c7e-fc75-11dd-ada4-5153b8187bf2>
2009-06-11 01:51:36 +0000

    * Created a somewhat correct Map object from the json returned from Warfish. It is hacked together right now as I am just trying to figure out what goes where. It needs to be done in a better way.

--- src/pyFish/Continent.py
+++ src/pyFish/Continent.py
@@ -24,6 +24,16 @@ class Continent:
         self._territories = territories
         assert bonus > 0, "The continent bonus must be greater than 0."
         self._bonus = bonus
+    
+    def __init__(self, continentDictionary, territories):
+        self._name = continentDictionary['name']
+        self._id = continentDictionary['id']
+        self._bonus = continentDictionary['units']
+        self._territories = []
+        for id in continentDictionary['cids'].split(','):
+            for territory in territories:
+                if territory.id == id:
+                    self._territories.append(territory)
         
     @property
     def name(self):
@@ -40,6 +50,11 @@ class Continent:
         """When a single player controls all territories on a continent at the 
         beginning of his turn he will receive bonus armies."""
         return self._bonus
+    
+    @property
+    def id(self):
+        """The id assigned to the continent by Warfish."""
+        return self._id
     
 if __name__ == "__main__":
     import doctest
